741494655a8d4f5e50dc838f461f165fe1352ee6
Ramy Anber <ramyanber@gmail.com>
2021-04-14 00:45:16 -0400

    Just menu

---
+++ Bank System/Bank System/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bank_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the bank system");
+            Console.WriteLine("Front Desk press 1");
+            Console.WriteLine("User press 2");
+            Console.WriteLine("Employee press 3");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine("welcome front desk");
+                    Class1 front = new Class1();
+                    front.getFrontDeskMenu();
+                    break;
+                case "2":
+                    Console.WriteLine("wlecome user");
+                    break;
+                case "3":
+                    Console.WriteLine("welcome Employee");
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+}
---
+++ Bank System/Bank System/getMenus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank_System
+{
+    class getMenus
+    {
+        
+    }
+}
